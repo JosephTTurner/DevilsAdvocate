53e467ac0d15fc4e3b9150e059d00de546129023
JosephTTurner <joe5@vt.edu>
2017-10-09 18:54:59 -0400

    parsing and example of using ajax to load a link

--- parse.js
+++ parse.js
@@ -4,17 +4,26 @@
 
 	'use strict';
 
-	// Include HTML
-	var container = document.querySelector('body');
+	// // Include HTML
+	// var container = document.querySelector('body');
 
-	$.get(chrome.extension.getURL('./content.html'), function(data) {
-		$($.parseHTML(data)).appendTo(container);
-	});
+	// $.get(chrome.extension.getURL('./content.html'), function(data) {
+	// 	$($.parseHTML(data)).appendTo(container);
+	// });
 
-	var el = document.createElement( 'html' );
-	el.innerHTML = "<html><head><title>titleTest</title></head><body><a href='test0'>test01</a><a href='test1'>test02</a><a href='test2'>test03</a></body></html>";
+	// var el = document.createElement( 'html' );
+	// el.innerHTML = "<html><head><title>titleTest</title></head><body><a href='test0'>test01</a><a href='test1'>test02</a><a href='test2'>test03</a></body></html>";
 
-	el.getElementsByTagName( 'a' ); 	
+	// el.getElementsByTagName( 'a' ); 
+
+
+	var arr = [], l = document.links;
+	for(var i=0; i < l.length; i++) {
+  		arr.push(l[i].href);
+	}	
 	
-
+	for(var i = 0; i < arr.length; i++){
+		console.log(arr[i]); 	
+	}
+	
 })(jQuery);
