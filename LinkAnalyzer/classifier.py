5382c94e2496882669e16ba672b263d8b84ffaca
juliae13 <julia.evans94@gmail.com>
2017-12-05 15:27:52 -0500

    Add files via upload

---
+++ LinkAnalyzer/classifier.py
@@ -0,0 +1,24 @@
+import nltk.corpus import word_tokenize
+import csv
+featuresets = []
+
+
+with open('./Fake_News.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        featuresets.append((nltk.word_tokenize(row['text']), 'Fake'))
+with open('./Real_Articles_Data_Clean_English_Test4.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        featuresets.append((nltk.word_tokenize(row['text']), 'Real'))
+#articleData = ([(articleText, 'fake') for articleText in .words('Fake.txt')] + [(articleText, 'real') for articleText in articleText.words('real.txt')])
+import random
+random.shuffle(featuresets)
+
+#featuresets = [(articleText, validity) for (n, validity) in articleData]
+train_set, test_set = featuresets[4430:], featuresets[:4430]
+print ('Made Data Sets')
+classifier = nltk.NaiveBayesClassifier.train(train_set)
+print('Made classifier')
+classifier.classify(article_features('exampleArticle.txt'))
+print(nltk.classify.accuracy(classifier, test_set))
