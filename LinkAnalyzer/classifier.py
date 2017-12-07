b93e176896ef3d309aca521ec112603fdebcdfdd
juliae13 <julia.evans94@gmail.com>
2017-12-06 21:07:34 -0500

    Removed unnecessary comments & added useful ones

--- LinkAnalyzer/classifier.py
+++ LinkAnalyzer/classifier.py
@@ -1,10 +1,12 @@
-
 import nltk.corpus
 from nltk.tokenize import word_tokenize
+from nltk.classify import SklearnClassifier
+from sklearn.naive_bayes import BernoulliNB
 import csv
 featuresets = []
 
-
+#import the csv files from the datasets and mark them as true or false given the data.
+#this will act as the trainset and test set
 with open('./Fake_News.csv') as csvfile:
 	reader = csv.DictReader(csvfile)
 	for row in reader:
@@ -20,20 +22,12 @@ with open('./Real_News.csv') as csvfile:
 			addSet = [{'word': word}, 'True']
 			featuresets.append(addSet)
 import random
-random.shuffle(featuresets)
-print (len(featuresets))
-train_set, test_set = featuresets[1258252:], featuresets[:1258253]
+random.shuffle(featuresets) #mix the data
+
+train_set, test_set = featuresets[1258252:], featuresets[:1258253] #The first half of the dataset will be used as the trainset and the other half will be used as the test set
 print ('Made Data Sets')
-classifier = nltk.NaiveBayesClassifier.train(train_set)
+classifier = SklearnClassifier(BernoulliNB()).train(train_set) #Create the classification using the train set
 print('Made classifier')
 testFile = {}
-# with open('exampleArticle.txt') as file:
-# 	for line in file:
-# 		articleWords = nltk.word_tokenize(line)
-# 		for word in articleWords:
-# 			addSet = [{'word': word}, 'False']
-# 			testFile.update(addSet)
 
-# classifier.classify(testFile)
-
-print(nltk.classify.accuracy(classifier, test_set))
+print(nltk.classify.accuracy(classifier, test_set)) #test the accuracy of the classifier on the test set
