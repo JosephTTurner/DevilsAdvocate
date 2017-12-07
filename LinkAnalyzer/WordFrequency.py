639a6a2383d79e51d2066f3a869abb3ea99e65c7
juliae13 <julia.evans94@gmail.com>
2017-12-06 21:09:42 -0500

    Added more comments

--- LinkAnalyzer/WordFrequency.py
+++ LinkAnalyzer/WordFrequency.py
@@ -3,19 +3,18 @@ import csv, sys, argparse
 articleData = []
 dictionary = {}
 csv.field_size_limit(5000000)
-with open('Real_Articles_Data_Clean_English_Test2.csv' , encoding= 'ISO-8859-1') as csvfile:
+with open('Real_News.csv' , encoding= 'ISO-8859-1') as csvfile:
     wordreader = csv.reader(csvfile, delimiter='\n', quotechar='|')
     entry = []
-    for row in wordreader:
+    for row in wordreader: #split the csv file into articles
         entry = row[0].split(',')
         articleData.append(entry)
 
 for entry in articleData:
-    articleData = entry[3].split()
+    articleData = entry[3].split() #split the articles into words
     for word in articleData:
         #normalize all of the words by removing all non-alphanumeric characters
         #and making all of the words lowercase
-        #word = re.sub(r'\W+', '', word)
         word = word.lower()
         if (word not in stopwords.words('english')): #Don't include stopwords
             if word not in dictionary: #check to see if the word has been added to the dictionary
@@ -23,6 +22,7 @@ for entry in articleData:
             else:
                 dictionary[word] = dictionary[word] + 1
 
+#Format the data into readable comments with headers
 sort = sorted(dictionary.items(), key = lambda x:x[1], reverse = True)
 print ('{:20}{:3}'.format('Word', 'Frequency'))
 for (word, frequency) in sort:
